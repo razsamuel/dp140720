07277c13afd86b92603979ed6ced2e935212a7f2
ryoyop <razek258@gmail.com>
2019-07-14 14:01:50 +0300

    Add files via upload

---
+++ Factory/Truck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Truck : Vehicle
+    {
+        protected string radioStations;
+
+        public Truck(string model, float cylinder, string color, int numberOfWheels, string radioStations) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.radioStations = radioStations;
+        }
+
+        public override string ToString()
+        {
+            return "Truck " + base.ToString() + $" {radioStations}";
+        }
+    }
+}
---
+++ Factory/Unicycle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Unicycle : Vehicle
+    {
+        protected string stability;
+
+        public Unicycle(string model, float cylinder, string color, int numberOfWheels, string stability) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.stability = stability;
+        }
+
+        public override string ToString()
+        {
+            return "Unicycle " + base.ToString() + $" {stability}";
+        }
+    }
+}
