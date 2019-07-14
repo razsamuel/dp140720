75170dd727eb71f9b707fe1ee0afa4cc5914debd
ryoyop <razek258@gmail.com>
2019-07-14 11:46:06 +0300

    Add files via upload

---
+++ Singleton/Clock.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Clock
+    {
+
+        private static Clock INSTANCE;
+
+        private static object key = new object();
+        private Clock()
+        {
+
+        }
+
+        public static Clock GetInstance()
+        {
+            if (INSTANCE != null)
+            {
+                return INSTANCE;
+            }
+
+            lock (key)
+            {
+                if (INSTANCE == null)
+                {
+                    INSTANCE = new Clock();
+                }
+            }
+
+            return INSTANCE;
+        }
+
+        public void printHour()
+        {
+            Console.WriteLine(DateTime.Now.ToString());
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Clock s1 = Clock.GetInstance();
+            Clock s2 = Clock.GetInstance();
+            Clock s3 = Clock.GetInstance();
+
+            s1.printHour();
+            s2.printHour();
+            s3.printHour();
+        }
+    }
+}
