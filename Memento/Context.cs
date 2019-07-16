c0acc9ca37fa2325c294c55d11de817afee96c6b
ryoyop <razek258@gmail.com>
2019-07-16 14:29:31 +0300

    Add files via upload

---
+++ Memento/Context.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memento
+{
+    public class Context
+    {
+
+        TextCareTaker careTake = new TextCareTaker();
+
+        public void Save(RichTextBox richTex)
+        {
+            careTake.AddMemento(new TextMemento(richTex.Text));
+        }
+
+        public void LoadPrev(RichTextBox richText)
+        {
+            if (careTake.GetLast(false) != null && careTake.GetLast(false).savedText != null)
+                richText.Text = careTake.GetLast(true).savedText;
+        }
+        public void LoadNext(RichTextBox richText)
+        {
+            if (careTake.getNext(false) != null && careTake.getNext(false).savedText != null)
+                richText.Text = careTake.getNext(true).savedText;
+        }
+    }
+}
---
+++ Memento/TextCareTaker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class TextCareTaker
+    {
+        List<TextMemento> allMementos = new List<TextMemento>();
+        int index = 0;
+        public void AddMemento(TextMemento memento)
+        {
+            if (allMementos.Count - 1 > index)
+            {
+                allMementos.RemoveRange(index, allMementos.Count - index);
+            }
+
+            allMementos.Add(memento);
+            index = allMementos.Count;
+        }
+
+        public void RemoveMemento()
+        {
+        }
+
+        public TextMemento GetLast(bool changeIndex)
+        {
+            if (index > 0)
+            {
+                TextMemento textMemento = allMementos[index - 1];
+                if (changeIndex)
+                {
+                    index -= 1;
+                }
+                return textMemento;
+
+            }
+            return new TextMemento("");
+        }
+
+        public TextMemento getNext(bool changeIndex)
+        {
+            if (index < allMementos.Count - 1)
+            {
+                TextMemento textMemento = allMementos[index + 1];
+                if (changeIndex)
+                {
+                    index += 1;
+                }
+                return textMemento;
+
+            }
+            return null;
+        }
+    }
+}
