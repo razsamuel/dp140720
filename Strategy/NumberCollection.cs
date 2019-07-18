b787d4b7c4f2e63f2fb3333b9592a3eaddaa6544
ryoyop <razek258@gmail.com>
2019-07-18 09:52:54 +0300

    Add files via upload

--- Strategy/NumberCollection.cs
+++ Strategy/NumberCollection.cs
@@ -6,23 +6,33 @@ using System.Threading.Tasks;
 
 namespace Strategy
 {
+
+    enum SortType
+    {
+        QUICK,
+        MERGE
+    }
     class NumberCollection
     {
         private List<int> numbers;
         private ISort sorter;
+        private SortType sortType;
 
         public NumberCollection()
         {
             numbers = new List<int>();
+            sorter = new MergeSort();
+            sortType = SortType.MERGE;
         }
 
         public void add(int number)
         {
             numbers.Add(number);
 
-            if (numbers.Count == 1000)
+            if (numbers.Count >= 1000 && sortType != SortType.QUICK)
             {
                 sorter = new QuickSort();
+                sortType = SortType.QUICK;
             }
         }
 
@@ -31,9 +41,10 @@ namespace Strategy
             numbers.Remove(number);
 
 
-            if (numbers.Count == 999)
+            if (numbers.Count < 1000 && sortType != SortType.MERGE)
             {
                 sorter = new MergeSort();
+                sortType = SortType.MERGE;
             }
         }
 
--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -10,6 +10,25 @@ namespace Strategy
     {
         static void Main(string[] args)
         {
+            NumberCollection numberCollection = new NumberCollection();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                numberCollection.add(1);
+                numberCollection.add(3);
+                numberCollection.add(2);
+            }
+            numberCollection.sort();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                numberCollection.remove(1);
+                numberCollection.remove(3);
+                numberCollection.remove(2);
+            }
+
+            numberCollection.sort();
+
         }
     }
 }
